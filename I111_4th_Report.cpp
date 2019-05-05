84a15a30994f53ebb01fecf8b5ca229176e94a16
sohoelf <sohoelf@gmail.com>
2019-05-06 02:41:29 +0900

    Add files via upload

---
+++ I111_4th_Report.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <random>
+#define n 150
+int main()
+{
+	short s[n - 1];
+	int a[101];
+	for (auto& i : a)i = 0;
+	for (auto& i : s)
+	{
+		i = rand() % 101;
+		a[i]++;
+	}
+	std::cout << a[91];
+	return 0;
+}
